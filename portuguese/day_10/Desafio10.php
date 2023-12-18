3e8f53d4a11df5249fab47f2e57f8cbe03271de5
Plínio Almeida <plinioalmeida20@gmail.com>
2023-12-17 22:49:08 -0300

    Day 10 - Parte 2
    não solucionado

--- portuguese/day_10/Desafio10.php
+++ portuguese/day_10/Desafio10.php
@@ -2,7 +2,7 @@
 
 class Desafio10
 {
-    private const MAPA = [
+    private const CAMINHOS = [
         '|' => [[0, -1], [0, 1]],
         '-' => [[-1, 0], [1, 0]],
         'L' => [[0, -1], [1, 0]],
@@ -15,12 +15,22 @@ class Desafio10
     /**
      * @var string[][]
      */
-    private array $mapa;
+    private array $mapaOriginal;
+
+    /**
+     * @var string[][][]
+     */
+    private array $mapas;
 
     /**
      * @var int[][]
      */
-    private array $mapaPercorrido;
+    private array $mapaPercorridoOriginal;
+
+    /**
+     * @var int[][][]
+     */
+    private array $mapasPercorridos;
     private int $startX;
     private int $startY;
     private int $x;
@@ -35,22 +45,22 @@ class Desafio10
     {
         $linhas = explode(PHP_EOL, file_get_contents($this->caminhoDoArquivo));
 
-        $this->mapa = [];
-        $this->mapaPercorrido = [];
+        $this->mapaOriginal = [];
+        $this->mapaPercorridoOriginal = [];
         $this->startX = -1;
         $this->startY = -1;
         foreach ($linhas as $y => $linha) {
             $colunas = str_split($linha);
 
             foreach ($colunas as $x => $coluna) {
-                $this->mapa[$x][$y] = $coluna;
-                $this->mapaPercorrido[$x][$y] = null;
+                $this->mapaOriginal[$x][$y] = $coluna;
+                $this->mapaPercorridoOriginal[$x][$y] = null;
 
                 if ($coluna === 'S') {
                     $this->startX = $x;
                     $this->startY = $y;
 
-                    $this->mapaPercorrido[$x][$y] = 0;
+                    $this->mapaPercorridoOriginal[$x][$y] = 0;
                 }
             }
         }
@@ -60,78 +70,159 @@ class Desafio10
 
     public function percorrerMapa(): int
     {
-        $this->substituirPecaInicio();
+        $this->prepararMapas();
 
-        $this->x = $this->startX;
-        $this->y = $this->startY;
+        $caminhos = [];
 
-        $caminho = 0;
+        foreach ($this->mapas as $i => $mapa) {
+//            echo PHP_EOL;
+            $this->x = $this->startX;
+            $this->y = $this->startY;
 
-        while (true) {
-            $peca = $this->obterPeca($this->x, $this->y);
-            $x1 = $this->x + self::MAPA[$peca][0][0];
-            $y1 = $this->y + self::MAPA[$peca][0][1];
-            $x2 = $this->x + self::MAPA[$peca][1][0];
-            $y2 = $this->y + self::MAPA[$peca][1][1];
+            $caminho = 0;
 
-            if (isset($this->mapaPercorrido[$x1][$y1]) && isset($this->mapaPercorrido[$x2][$y2])) {
-                break;
+            while (true) {
+                $peca = $this->obterPeca($i, $this->x, $this->y);
+//                print_r($peca);
+
+                $x1 = $this->x + self::CAMINHOS[$peca][0][0];
+                $y1 = $this->y + self::CAMINHOS[$peca][0][1];
+                $x2 = $this->x + self::CAMINHOS[$peca][1][0];
+                $y2 = $this->y + self::CAMINHOS[$peca][1][1];
+
+                if (isset($this->mapasPercorridos[$i][$x1][$y1]) && isset($this->mapasPercorridos[$i][$x2][$y2])) {
+                    break;
+                }
+
+                if (!isset($this->mapasPercorridos[$i][$x1][$y1])) {
+                    $this->x = $x1;
+                    $this->y = $y1;
+                } elseif (!isset($this->mapasPercorridos[$i][$x2][$y2])) {
+                    $this->x = $x2;
+                    $this->y = $y2;
+                } else {
+                    $caminho = -1;
+                    break;
+                }
+
+                $caminho++;
+                $this->mapasPercorridos[$i][$this->x][$this->y] = $caminho;
             }
 
-
-            if (!isset($this->mapaPercorrido[$x1][$y1])) {
-                $this->x = $x1;
-                $this->y = $y1;
+            if (($x1 === $this->startX && $y1 === $this->startY) || ($x2 === $this->startX && $y2 === $this->startY)) {
+                $caminhos[] = $caminho;
             } else {
-                $this->x = $x2;
-                $this->y = $y2;
+                unset($this->mapas[$i]);
+                unset($this->mapasPercorridos[$i]);
             }
-
-            $caminho++;
-            $this->mapaPercorrido[$this->x][$this->y] = $caminho;
+//            echo PHP_EOL;
         }
 
         $this->imprimirTempoGasto('percorrer mapa');
 
-        return ceil($caminho / 2);
+        print_r($caminhos);
+
+        return ceil(max($caminhos) / 2);
     }
 
-    private function substituirPecaInicio(): void
+    public function contarAreaInterna(): int
     {
-        $peca = $this->obterPeca($this->startX, $this->startY - 1);
-        $peca2 = $this->obterPeca($this->startX - 1, $this->startY);
-        if ($peca !== null && $peca2 !== null) {
-            $this->mapa[$this->startX][$this->startY] = 'J';
-        }
-        $peca2 = $this->obterPeca($this->startX + 1, $this->startY);
-        if ($peca !== null && $peca2 !== null) {
-            $this->mapa[$this->startX][$this->startY] = 'L';
-        }
-        $peca2 = $this->obterPeca($this->startX, $this->startY + 1);
-        if ($peca !== null && $peca2 !== null) {
-            $this->mapa[$this->startX][$this->startY] = '|';
+        $areasInternas = [];
+        foreach ($this->mapas as $i => $mapa) {
+            $this->imprimirMapa($i);
+
+            $areaInterna = 0;
+
+            for ($y = 0; $y < count($this->mapaOriginal[0]); $y++) {
+                for ($x = 0; $x < count($this->mapaOriginal); $x++) {
+                    if (in_array($this->mapas[$i][$x][$y], ['7', 'F', 'J', 'L', '-', '|', '#'])) {
+                        continue;
+                    }
+
+                    $areaInterna++;
+                }
+            }
+//            for ($x = 1; $x < count($this->mapaOriginal) - 1; $x++) {
+//                for ($y = 1; $y < count($this->mapaOriginal[$x]) - 1; $y++) {
+//                    if (isset($this->mapasPercorridos[$i][$x][$y])) {
+//                        continue;
+//                    }
+//
+//                    $aberturasAEsquerda = $this->contarAberturasAEsquerda($i, $x, $y);
+//                    $aberturasAcima = $this->contarAberturasAcima($i, $x, $y);
+//
+//                    if ((($aberturasAEsquerda % 2) === 1) && (($aberturasAcima % 2) === 1)) {
+////                        echo implode(' - ', [$i, $x, $y, $aberturasAEsquerda, $aberturasAcima]) . PHP_EOL;
+//                        $areaInterna++;
+////                    echo $x . ' ' . $y . PHP_EOL;
+//                    }
+//                }
+//            }
+
+            $this->imprimirTempoGasto('percorrer area interna');
+
+            $areasInternas[] = $areaInterna;
         }
 
-        $peca = $this->obterPeca($this->startX - 1, $this->startY);
-        $peca2 = $this->obterPeca($this->startX + 1, $this->startY);
+        print_r($areasInternas);
+
+        return $areasInternas[0];
+    }
+
+    private function prepararMapas(): void
+    {
+        $pecas = [];
+
+        $peca = $this->obterPeca(null, $this->startX, $this->startY - 1);
+        $peca2 = $this->obterPeca(null, $this->startX - 1, $this->startY);
         if ($peca !== null && $peca2 !== null) {
-            $this->mapa[$this->startX][$this->startY] = '-';
+            $pecas[] = 'J';
         }
-        $peca2 = $this->obterPeca($this->startX, $this->startY + 1);
+        $peca2 = $this->obterPeca(null, $this->startX + 1, $this->startY);
         if ($peca !== null && $peca2 !== null) {
-            $this->mapa[$this->startX][$this->startY] = '7';
+            $pecas[] = 'L';
+        }
+        $peca2 = $this->obterPeca(null, $this->startX, $this->startY + 1);
+        if ($peca !== null && $peca2 !== null) {
+            $pecas[] = '|';
         }
 
-        $peca = $this->obterPeca($this->startX + 1, $this->startY);
-        $peca2 = $this->obterPeca($this->startX, $this->startY + 1);
+        $peca = $this->obterPeca(null, $this->startX - 1, $this->startY);
+        $peca2 = $this->obterPeca(null, $this->startX + 1, $this->startY);
         if ($peca !== null && $peca2 !== null) {
-            $this->mapa[$this->startX][$this->startY] = 'F';
+            $pecas[] = '-';
+        }
+        $peca2 = $this->obterPeca(null, $this->startX, $this->startY + 1);
+        if ($peca !== null && $peca2 !== null) {
+            $pecas[] = '7';
+        }
+
+        $peca = $this->obterPeca(null, $this->startX + 1, $this->startY);
+        $peca2 = $this->obterPeca(null, $this->startX, $this->startY + 1);
+        if ($peca !== null && $peca2 !== null) {
+            $pecas[] = 'F';
+        }
+
+        $this->mapas = [];
+        $this->mapasPercorridos = [];
+
+        foreach ($pecas as $peca) {
+            $this->mapaOriginal[$this->startX][$this->startY] = $peca;
+
+            $this->mapas[] = $this->mapaOriginal;
+            $this->mapasPercorridos[] = $this->mapaPercorridoOriginal;
         }
     }
 
-    private function obterPeca(int $x, int $y): ?string
+    private function obterPeca(?int $indice, int $x, int $y): ?string
     {
-        return $this->mapa[$x][$y] ?? null;
+        if ($indice !== null) {
+            $peca = $this->mapas[$indice][$x][$y] ?? null;
+        } else {
+            $peca = $this->mapaOriginal[$x][$y] ?? null;
+        }
+
+        return $peca === '.' ? null : $peca;
     }
 
     private function imprimirTempoGasto(string $acao): void
@@ -139,5 +230,203 @@ class Desafio10
         $agora = microtime(true);
 
         echo sprintf('%.4f segundos se passaram para %s', $agora - $this->inicio, $acao) . PHP_EOL;
+    }
+
+    private function contarAberturasAEsquerda(int $indice, int $x, int $y): int
+    {
+//        echo "($x $y)" . PHP_EOL;
+        $sequencia = '';
+        for ($i = 0; $i < $x; $i++) {
+            if (!isset($this->mapasPercorridos[$indice][$i][$y])) {
+                continue;
+            }
+
+            $peca = $this->mapas[$indice][$i][$y];
+            if ($peca === '-') {
+                continue;
+            }
+
+            $sequencia .= $peca;
+        }
+
+        $sequencia = str_replace('FJ', '|', $sequencia);
+        $sequencia = str_replace('L7', '|', $sequencia);
+
+//        echo $sequencia . PHP_EOL;
+
+        return strlen($sequencia);
+    }
+
+    private function contarAberturasAcima(int $indice, int $x, int $y): int
+    {
+//        echo "($x $y)" . PHP_EOL;
+        $sequencia = '';
+        for ($i = 0; $i < $y; $i++) {
+            if (!isset($this->mapasPercorridos[$indice][$x][$i])) {
+                continue;
+            }
+
+            $peca = $this->mapas[$indice][$x][$i];
+            if ($peca === '|') {
+                continue;
+            }
+
+            $sequencia .= $peca;
+        }
+
+        $sequencia = str_replace('FJ', '-', $sequencia);
+        $sequencia = str_replace('L7', '-', $sequencia);
+
+//        echo $sequencia . PHP_EOL;
+
+        return strlen($sequencia);
+    }
+
+    private function imprimirMapa(int $i): void
+    {
+        echo '  - ';
+        for ($x = 0; $x < count($this->mapaOriginal); $x++) {
+            echo substr($x, -1) . ' ';
+        }
+
+        echo PHP_EOL;
+        $substituiu = true;
+        while ($substituiu) {
+            $substituiu = false;
+            for ($y = 0; $y < count($this->mapaOriginal[0]); $y++) {
+                for ($x = 0; $x < count($this->mapaOriginal); $x++) {
+                    if (isset($this->mapasPercorridos[$i][$x][$y])) {
+                        if ($this->substituirCantos($i, $x, $y)) {
+                            $y++;
+                            $substituiu = true;
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        for ($y = 0; $y < count($this->mapaOriginal[0]); $y++) {
+            for ($x = 1; $x < count($this->mapaOriginal); $x++) {
+                $this->mapas[$i][$x - 1][$y] = '#';
+                $this->mapasPercorridos[$i][$x - 1][$y] = 1;
+
+                if (in_array($this->mapas[$i][$x][$y], ['7', 'F', 'J', 'L', '-', '|'])) {
+                    break;
+                }
+            }
+            for ($x = count($this->mapaOriginal) - 2; $x >= 0; $x--) {
+                $this->mapas[$i][$x + 1][$y] = '#';
+                $this->mapasPercorridos[$i][$x + 1][$y] = 1;
+
+                if (in_array($this->mapas[$i][$x][$y], ['7', 'F', 'J', 'L', '-', '|'])) {
+                    break;
+                }
+            }
+        }
+
+        for ($y = 0; $y < count($this->mapaOriginal[0]); $y++) {
+            echo $y . ' - ';
+            for ($x = 0; $x < count($this->mapaOriginal); $x++) {
+                echo (isset($this->mapasPercorridos[$i][$x][$y]) ? $this->mapas[$i][$x][$y] : ' ') . ' ';
+//                echo ($this->mapasPercorridos[$i][$x][$y] ?? ' ') . ' ';
+//                echo (isset($this->mapasPercorridos[$i][$x][$y]) ? '#' : ' ') . ' ';
+
+//                if (isset($this->mapasPercorridos[$i][$x][$y])) {
+//                    $this->mapasPercorridos[$i][$x][$y] = 'X';
+//                }
+            }
+            echo PHP_EOL;
+        }
+
+        echo '-----' . PHP_EOL;
+    }
+
+    private function substituirCantos(int $i, int $x, int $y): bool
+    {
+        $peca = $this->obterPeca($i, $x, $y);
+        $peca2 = $this->obterPeca($i, $x + 1, $y);
+        $peca3 = $this->obterPeca($i, $x, $y + 1);
+        $peca4 = $this->obterPeca($i, $x + 1, $y + 1);
+
+        $aux = $peca . $peca2 . $peca3 . $peca4;
+
+        if ($this->substituirString($i, $x, $y, $aux, 'FJL-', ' | L')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'L7-J', '| J ')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, '||LJ', 'LJ  ')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, '|FLJ', 'L-  ')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'FJL7', ' | |')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'F-L-', ' F L')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'F-L7', ' F |')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'F7|L', '  F-')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'F7JL', '  --')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, '7|LJ', '-J  ')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'F7||', '  F7')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, 'F7J|', '  -7')) {
+            return true;
+        }
+        if ($this->substituirString($i, $x, $y, $aux, '7FLJ', '--  ')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function substituirString(
+        int $i,
+        int $x,
+        int $y,
+        string $aux,
+        string $stringOrigem,
+        string $stringDestino
+    ): bool {
+        if ($aux === $stringOrigem) {
+            $incrementos = [
+                [0, 0],
+                [1, 0],
+                [0, 1],
+                [1, 1],
+            ];
+
+            $novosValores = str_split($stringDestino);
+            foreach ($incrementos as $k => $incremento) {
+                if ($novosValores[$k] === ' ') {
+                    $this->mapas[$i][$x + $incremento[0]][$y + $incremento[1]] = '#';
+                    $this->mapasPercorridos[$i][$x + $incremento[0]][$y + $incremento[1]] = 1;
+
+                    continue;
+                }
+
+                $this->mapas[$i][$x + $incremento[0]][$y + $incremento[1]] = $novosValores[$k];
+                $this->mapasPercorridos[$i][$x + $incremento[0]][$y + $incremento[1]] = 1;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
--- portuguese/day_10/day_10.php
+++ portuguese/day_10/day_10.php
@@ -6,8 +6,16 @@ spl_autoload_register(function ($class_name) {
 
 //$desafio = new Desafio10('example_puzzle_input_1.txt');
 //$desafio = new Desafio10('example_puzzle_input_2.txt');
+//$desafio = new Desafio10('example_puzzle_input_3.txt');
+//$desafio = new Desafio10('example_puzzle_input_4.txt');
 $desafio = new Desafio10('puzzle_input.txt');
 $desafio->lerArquivo();
 
 $pontoMaisDistante = $desafio->percorrerMapa();
 echo sprintf('O ponto mais distante fica a %d passos', $pontoMaisDistante) . PHP_EOL;
+
+$areaInterna = $desafio->contarAreaInterna();
+echo sprintf('A area interna do mapa tem %d blocos', $areaInterna) . PHP_EOL;
+//504 (too high)
+//138 (too low)
+//494 (too high)
