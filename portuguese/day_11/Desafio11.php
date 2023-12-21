7e23c9489654aa75a2b0fe5d65a5c3b3f62dce3e
Plínio Almeida <plinioalmeida20@gmail.com>
2023-12-21 01:12:09 -0300

    Day 11 - Parte 2
    30 minutos

--- portuguese/day_11/Desafio11.php
+++ portuguese/day_11/Desafio11.php
@@ -44,6 +44,16 @@ class Desafio11
         $this->imprimirTempoGasto('expandir universo');
     }
 
+    public function expandirUniverso2(): void
+    {
+        $this->expandirLinhas2();
+        $this->transpor();
+        $this->expandirLinhas2();
+        $this->transpor();
+
+        $this->imprimirTempoGasto('expandir universo');
+    }
+
     public function expandirLinhas(): void
     {
         $novoUniverso = [];
@@ -54,6 +64,22 @@ class Desafio11
             }
 
             $novoUniverso[] = $linha;
+        }
+
+        $this->universo = $novoUniverso;
+    }
+
+    public function expandirLinhas2(): void
+    {
+        $novoUniverso = [];
+        foreach ($this->universo as $linha) {
+            if (in_array('#', $linha)) {
+                $novoUniverso[] = $linha;
+
+                continue;
+            }
+
+            $novoUniverso[] = str_split(str_replace('.', 'X', implode('', $linha)));
         }
 
         $this->universo = $novoUniverso;
@@ -89,6 +115,59 @@ class Desafio11
                     + abs($this->galaxias[$i][1] - $this->galaxias[$j][1]);
             }
         }
+
+        $this->imprimirTempoGasto('calcular menor caminho');
+
+        return array_sum($distancias);
+    }
+
+    public function calcularMenorCaminho2(int $tamanhoExpansao): int
+    {
+        $this->galaxias = [];
+        foreach ($this->universo as $y => $linha) {
+            foreach ($linha as $x => $coluna) {
+                if ($coluna === '#') {
+                    $this->galaxias[] = [$y, $x];
+                }
+            }
+        }
+
+        echo sprintf('encontradas %d galaxias', count($this->galaxias)) . PHP_EOL;
+
+        $distancias = [];
+
+        for ($i = 0; $i < count($this->galaxias) - 1; $i++) {
+            echo $i . ' ' . PHP_EOL;
+            for ($j = $i + 1; $j < count($this->galaxias); $j++) {
+                list ($y1, $x1) = $this->galaxias[$i];
+                list ($y2, $x2) = $this->galaxias[$j];
+
+                $linha = substr(
+                    implode('', $this->universo[$y1]),
+                    $x1 < $x2 ? $x1 : $x2,
+                    abs($x2 - $x1)
+                );
+//                echo $linha . PHP_EOL;
+                $tamanhoLinha = (substr_count($linha, 'X') * $tamanhoExpansao)
+                    + strlen(str_replace('X', '', $linha));
+
+                $this->transpor();
+
+                $coluna = substr(
+                    implode('', $this->universo[$x1]),
+                    $y1 < $y2 ? $y1 : $y2,
+                    abs($y2 - $y1)
+                );
+//                echo $coluna . PHP_EOL;
+                $tamanhoColuna = (substr_count($coluna, 'X') * $tamanhoExpansao)
+                    + strlen(str_replace('X', '', $coluna));
+                $this->transpor();
+
+                $distancias[] = $tamanhoLinha + $tamanhoColuna;
+            }
+        }
+
+        $this->imprimirTempoGasto('calcular menor caminho');
 
         return array_sum($distancias);
     }
--- portuguese/day_11/day_11.php
+++ portuguese/day_11/day_11.php
@@ -5,9 +5,14 @@ spl_autoload_register(function ($class_name) {
 });
 
 //$desafio = new Desafio11('example_puzzle_input_1.txt');
+//$desafio = new Desafio11('example_puzzle_input_2.txt');
 $desafio = new Desafio11('puzzle_input.txt');
 $desafio->lerArquivo();
 $desafio->expandirUniverso();
-
 $somaDistancias = $desafio->calcularMenorCaminho();
+echo sprintf('A soma das menores distâncias é %d', $somaDistancias) . PHP_EOL;
+
+$desafio->lerArquivo();
+$desafio->expandirUniverso2();
+$somaDistancias = $desafio->calcularMenorCaminho2(1000000);
 echo sprintf('A soma das menores distâncias é %d', $somaDistancias) . PHP_EOL;
